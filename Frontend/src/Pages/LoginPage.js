8d78956885501619dfe1a121ac34484ba737ca5f
Rafayed Anwar <rafayedanwar2@gmail.com>
2025-12-05 12:58:20 +0600

    Added login page with routing to Dashboard

---
+++ Frontend/src/Pages/LoginPage.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import './CSS/loginPage.css'
+import Icon from  './Assets/icon.png'
+
+
+const LoginPage = () => {
+  return (
+    //whole page container
+    <div className="login-container">
+      <div className="login-left">
+          <div className="login-form">
+            <img src={Icon} alt="Icon" className="login-icon" />
+            <h2>Login with G-Suite</h2>
+            <p>If you don't have an account we will add one for you</p>
+            {/* Google Login Button */}
+            <button 
+                className="google-login-btn"
+                onClick={() => window.location.href = 'http://localhost:1688/api/auth/google'}>
+                <img src="https://www.svgrepo.com/show/475656/google-color.svg" alt="Google" />
+                Sign in with GSuite
+            </button>
+
+            <div className="divider">To maintain the relevance of our community and prevent lurkers, sign-up is only allowed using a BRACU email</div>
+          </div>
+      </div>
+
+      <div className="login-right">
+        <div className="login-content">
+          <h1><span className="blue">Uni</span>Community</h1>
+          <p>A community for BRAC University students</p>
+        </div>
+      </div>
+
+      
+    
+    </div>
+  )
+}
+
+//this line is missing - export name should match component name (LoginPage)
+export default LoginPage
