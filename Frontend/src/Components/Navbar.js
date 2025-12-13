a3529df96590066a23d0029c409312f39918a94c
Mahdi <mahdi.ndc21@gmail.com>
2025-12-13 21:43:42 +0600

    main branch clean-some issues in discussion need fixing

--- Frontend/src/Components/Navbar.js
+++ Frontend/src/Components/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = () => {
     <nav className="navbar">
       <div className="navbar-container">
         <Link
-          to="/"
+          to="/profile"
           className="navbar-logo"
           style={{ display: "flex", alignItems: "center", gap: "10px" }}
         >
